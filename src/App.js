f06001ef6d141160a0ad131313c324e179cbfb22
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-07-18 18:05:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import _ from 'lodash';
+
+//Test component
+class App extends Component {
+
+  renderChallenges() {
+    const challenges = this.props.data.allChallenges;
+
+    return _.map(challenges, (challenge) => {
+      return(
+        <div key={challenge.id}>
+           <h1>{challenge.name}</h1>
+           <p>{challenge.shortDescription}</p>
+        </div>
+      );
+    });
+  }
+
+  render() {
+
+    const {loading, error } = this.props.data;
+
+    if (loading) {
+      return(
+        <div className="App">
+          <h1>{'Loading...'}</h1>
+        </div>
+      );
+    }
+
+    if (error) {
+      return(
+        <div className="App">
+          <h1>{'Error'}</h1>
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>{'Total challenges: ' + this.props.data._allChallengesMeta.count}</h1>
+        {this.renderChallenges()}
+      </div>
+    );
+  }
+}
+
+export const allChallenges = gql`
+  query allChallenges {
+    allChallenges {
+      id
+      name
+      shortDescription
+    },
+    _allChallengesMeta {
+      count
+    }
+  }
+`;
+
+export default graphql(allChallenges)(App);
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+//GraphCMS imports
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+const GRAPHCMS_API = 'https://api.graphcms.com/simple/v1/cjiyie4xo1k0x0149nyl9lqvt';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>, 
+	document.getElementById('root')
+);
+registerServiceWorker();
