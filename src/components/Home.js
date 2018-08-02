202aae05b04ab52ca11356cfce151639734bc990
Petrus Holm <holm.petrus@gmail.com>
2018-08-03 02:50:23 +0300

    styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,31 @@ const Home = ({ data: { loading, error, allChallenges } }) => {
   if (error) return <h1>Error fetching challenges!</h1>;
   if (!loading) {
     return (
-      <section>
-        <ul className="Home-ul">
-          {allChallenges.map(challenge => (
-            <li className="Home-li" key={`challenge-${challenge.id}`}>
-              <Link to={`/challenge/${challenge.id}`} className="Home-link">
-                <div className="Home-placeholder" />
-                <h3>{challenge.name}</h3>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className="container">
+        <div className="row">
+          <div className="grid">
+            {allChallenges.map(challenge => (
+              <div className="grid-item" key={`challenge-${challenge.id}`}>
+                <div className="grid-content">
+                  <div className="flexrow">
+                    <p className="category">Category</p>
+                    <p className="difficulty">Difficulty</p>
+                  </div>
+                  <h5>
+                    <b>{challenge.name}</b>
+                  </h5>
+                  <p>{challenge.shortDescription}</p>
+                </div>
+                <div className="flexrow">
+                  <Link to={`/challenge/${challenge.id}`} className="grid-link">
+                    See Details >
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     );
   }
   return <h2>Loading challenges...</h2>;
