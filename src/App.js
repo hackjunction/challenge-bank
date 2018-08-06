4b84eaa1f73959383d3c85e86b4a37e01e7d1465
kymis <erik.kymalainen@gmail.com>
2018-08-06 22:13:10 +0300

    filtering by difficulty

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./components/Home/";
 import Challenge from "./components/Challenge";
 import Cities from "./components/Cities";
 
