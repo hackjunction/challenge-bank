db041b561bddfd8d1d71afbb37909941738f59cf
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-08-28 15:48:17 +0300

    add path import to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     bluebird = require('bluebird'),
+    path = require('path'),
     port = process.env.PORT || 3000;
 
 /* Set mongoose  & global to use Bluebird promises */
