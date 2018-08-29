452cbd403b32508e20318ff63d4c96505f5aa7de
Petrus Holm <holm.petrus@gmail.com>
2018-08-29 18:59:49 +0300

    added react-table module, changed mongodb uri

--- client/src/components/admin/SubmissionsList/index.js
+++ client/src/components/admin/SubmissionsList/index.js
@@ -7,96 +7,96 @@ import './style.css';
 import SubmissionsTable from './SubmissionsTable';
 
 class SubmissionsList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            submissions: []
-        };
+    this.state = {
+      loading: false,
+      submissions: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data && !nextProps.data.loading) {
+      this.getSubmissions();
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.data && !nextProps.data.loading) {
-            this.getSubmissions();
-        }
-    }
+  async getSubmissions() {
+    this.setState({
+      loading: true
+    });
 
-    async getSubmissions() {
+    try {
+      const response = await fetch('/api/submissions');
+      const body = await response.json();
+
+      if (body.status === 'success') {
         this.setState({
-            loading: true
+          loading: false,
+          submissions: this.mapSubmissionsToChallenges(body.data),
+          error: null
         });
-
-        try {
-            const response = await fetch('/api/submissions');
-            const body = await response.json();
-
-            if (body.status === 'success') {
-                this.setState({
-                    loading: false,
-                    submissions: this.mapSubmissionsToChallenges(body.data),
-                    error: null
-                });
-            } else {
-                this.setState({
-                    loading: false,
-                    error: body.data
-                });
-            }
-        } catch (error) {
-            console.log(error);
-            this.setState({
-                loading: false,
-                error: error
-            });
-        }
-    }
-
-    mapSubmissionsToChallenges(submissions) {
-        const mapped = _.map(submissions, submission => {
-            const challenge = _.find(this.props.data.allChallenges, challenge => {
-                return challenge.id === submission.challengeId;
-            });
-
-            submission.challenge = challenge;
-            return submission;
+      } else {
+        this.setState({
+          loading: false,
+          error: body.data
         });
-
-        return _.filter(mapped, m => typeof m.challenge != 'undefined');
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: error
+      });
     }
+  }
 
-    render() {
-        return (
-            <div className="SubmissionsList--container">
-                <div className="SubmissionsTable--wrapper">
-                    <h1 className="SubmissionsList--title">Submissions</h1>
-                    <SubmissionsTable data={this.state.submissions} />
-                </div>
-            </div>
-        );
-    }
+  mapSubmissionsToChallenges(submissions) {
+    const mapped = _.map(submissions, submission => {
+      const challenge = _.find(this.props.data.allChallenges, challenge => {
+        return challenge.id === submission.challengeId;
+      });
+
+      submission.challenge = challenge;
+      return submission;
+    });
+
+    return _.filter(mapped, m => typeof m.challenge != 'undefined');
+  }
+
+  render() {
+    return (
+      <div className="SubmissionsList--container">
+        <div className="SubmissionsTable--wrapper">
+          <h1 className="SubmissionsList--title">Submissions</h1>
+          <SubmissionsTable data={this.state.submissions} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export const allChallenges = gql`
-    query allChallenges {
-        allChallenges {
-            id
-            name
-            shortDescription
-            challengeCategory {
-                color
-                name
-            }
-            challengeDifficulty {
-                name
-                difficultyvalue
-            }
-            answer
-        }
-        _allChallengesMeta {
-            count
-        }
+  query allChallenges {
+    allChallenges {
+      id
+      name
+      shortDescription
+      challengeCategory {
+        color
+        name
+      }
+      challengeDifficulty {
+        name
+        difficultyvalue
+      }
+      answer
     }
+    _allChallengesMeta {
+      count
+    }
+  }
 `;
 
 export default graphql(allChallenges)(SubmissionsList);
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
-    bluebird = require('bluebird'),
-    path = require('path'),
-    port = process.env.PORT || 3000;
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  bluebird = require('bluebird'),
+  path = require('path'),
+  port = process.env.PORT || 3000;
 
 /* Set mongoose  & global to use Bluebird promises */
 global.Promise = bluebird;
 mongoose.Promise = bluebird;
-mongoose.connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/challengeBank');
+mongoose.connect(
+  process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : 'mongodb://heroku_kpj537v3:2kb09vf00v7qd9baudppe28l7d@ds227325.mlab.com:27325/heroku_kpj537v3'
+);
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -23,19 +27,19 @@ require('./api/models/Event');
 require('./api/models/Submission');
 
 app.get('/api/', function(req, res) {
-    res.send({
-        message: 'Hello from the API'
-    });
+  res.send({
+    message: 'Hello from the API'
+  });
 });
 
 // React config for production
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port);
