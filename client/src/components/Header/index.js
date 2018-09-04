40db201ffaa3d1c43a4fd3ca18318f15f8084313
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-09-04 19:37:00 +0300

    fix Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,35 +3,41 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-const Header = props => (
-  <div className="header">
-    <div className="junctionlogo" />
-    <div className="row userdata">
-      {props.user && props.user.username ? (
-        <React.Fragment>
-          <p>Logged in as: {props.user.username}</p>
-          <a href="/logout">Log out</a>
-        </React.Fragment>
-      ) : (
-        <a href="/login">Log in</a>
-      )}
-    </div>
-    <div className="container">
-      <div className="row">
-        <div className="header-content">
-          <Link to={`/`}>
-            <h2>Challenge Bank</h2>
-          </Link>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            risus lorem, commodo fringilla urna in, venenatis imperdiet mi.
-          </p>
-        </div>
-    </div>
-);
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="junctionlogo" />
+                <div className="row userdata">
+                    {props.user && props.user.username ? (
+                        <React.Fragment>
+                            <p>Logged in as: {props.user.username}</p>
+                            <a href="/logout">Log out</a>
+                        </React.Fragment>
+                    ) : (
+                        <a href="/login">Log in</a>
+                    )}
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="header-content">
+                            <Link to={`/`}>
+                                <h2>Challenge Bank</h2>
+                            </Link>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus lorem, commodo
+                                fringilla urna in, venenatis imperdiet mi.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = state => ({
-  user: state.user.user
+    user: state.user.user
 });
 
 export default connect(mapStateToProps)(Header);
